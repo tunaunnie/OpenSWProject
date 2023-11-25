4f10508e3ad5c248e208a3f8576d1b66a5c5995f
tunaunnie <ygw021227@gmail.com>
2023-11-25 10:43:18 +0000

    231125(2)

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database import DBhandler
 import sys
+import hashlib
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
+DB = DBhandler()
 
 @application.route("/")
 def hello():
@@ -38,14 +42,34 @@ def reg_item_submit():
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
-    
     image_file=request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
     data=request.form
+    DB.insert_item(data['name'], data, image_file.filename)
     
     return render_template("submit_item_result.html", data=data,
  img_path="static/images/{}".format(image_file.filename))
 
-   
+
+@application.route("/signup")
+def login():
+    return render_template("login.html")
+
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    
+    if DB.insert_user(data,pw_hash):    #id 중복체크
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("signup.html")
+    
 if __name__ == "__main__":
      application.run(host='0.0.0.0', debug=True)
